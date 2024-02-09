eb8fd968e9ef43d139e2e57e43722e0189a78148
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-08 21:10:12 -0300

    unificacao do script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,38 @@
+const graficoDolar = document.getElementById('graficoDolar');
+
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: 'Dolar',
+      data: [],
+      borderWidth: 1
+    }]
+  }
+});
+
+setInterval(() => conectaAPI(), 5000);
+
 async function conectaAPI() {
   const conecta = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL");
   const conectaTraduzido = await conecta.json();
-  console.log(conectaTraduzido);
+  let tempo = geraHorario();
+  let valor = conectaTraduzido.USDBRL.ask;
+  adicionarDados(graficoParaDolar, tempo, valor);
 }
 
-setInterval(() => conectaAPI(), 5000);
+function geraHorario() {
+  let data = new Date();
+  let horario = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  console.log(horario);
+  return horario;
+}
+
+function adicionarDados(grafico, legenda, dados){
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach(dataset => {
+    dataset.data.push(dados);
+  })
+  grafico.update();
+}
